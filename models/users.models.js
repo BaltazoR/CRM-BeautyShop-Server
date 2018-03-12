6490ee05f75b93ad194a626ad18d2d8644db5266
BaltazoR <baltazorich@gmail.com>
2018-03-12 21:11:21 +0200

    extension of the model

--- models/users.models.js
+++ models/users.models.js
@@ -4,13 +4,17 @@ let bcrtypt = require('bcryptjs');
 let userSchema = new mongoose.Schema({
     email: { type: String, unique: true, required: true },
     name: { type: String, required: true },
+    phoneNumber: { type: String, unique: true, required: true },
     password: String,
+    role: { type: String, required: true },
+    avatar: { type: String, default: 'default.jpg' },
+    userInfo: { type: String },
     addedAt: { type: Date, default: Date.now },
-    ip: {type: String}
+    ip: { type: String }
 });
 
-userSchema.pre('save', function(next) {
-    if(this.isModified('password') || this.isNew()) this.password = bcrtypt.hashSync(this.password, 12);
+userSchema.pre('save', function (next) {
+    if (this.isModified('password') || this.isNew()) this.password = bcrtypt.hashSync(this.password, 12);
     next();
 });
 
