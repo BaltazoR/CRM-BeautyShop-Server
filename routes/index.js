b5522c4c50b1e025ec7631fc318c36730609f4eb
BaltazoR <baltazorich@gmail.com>
2018-05-01 22:59:03 +0300

    new link to frontend

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,8 @@ let router = express.Router();
 
 router.get('/', function (req, res) {
     //res.sendStatus(200);
-    res.redirect('http://beauty-shop.s3-website.eu-central-1.amazonaws.com/');
+    //res.redirect('http://beauty-shop.s3-website.eu-central-1.amazonaws.com/');
+    res.redirect('https://d2z0iokdukqe3z.cloudfront.net/');
 });
 
 
